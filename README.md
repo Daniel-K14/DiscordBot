228969fb06dc7b6211f918cb5c06453b0b6d65eb
Substant14 <danielkokoszka@hotmail.com>
2017-09-09 17:00:25 +1000

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DiscordBot
+PUBG Stats Bot
---
+++ index.js
@@ -0,0 +1,4 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.login('MzQxNzk5MzI4MjI0MTE2NzM2.DJUi6g.kD2q0zeFla_sh5cF30GePPrOmNE');
